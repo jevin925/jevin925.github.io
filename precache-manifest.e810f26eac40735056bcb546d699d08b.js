514cdb0528b2f6b568f1ead97fbffd821200d96d
Jevin Vekaria <jevin925@gmail.com>
2020-07-29 15:51:30 +0530

    ninth commit

---
+++ precache-manifest.e810f26eac40735056bcb546d699d08b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0039f707ab95e1dac7bf948202c10d",
+    "url": "jevin925.github.io/index.html"
+  },
+  {
+    "revision": "6a6ef53e945932c9e0cd",
+    "url": "jevin925.github.io/static/css/3.48a93ee6.chunk.css"
+  },
+  {
+    "revision": "dab26a03637721888df5",
+    "url": "jevin925.github.io/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "ab1967473d5317d63ae9",
+    "url": "jevin925.github.io/static/css/main.6fae7221.chunk.css"
+  },
+  {
+    "revision": "76c32c743f29df5f8c9c",
+    "url": "jevin925.github.io/static/js/2.bde7e412.chunk.js"
+  },
+  {
+    "revision": "6a6ef53e945932c9e0cd",
+    "url": "jevin925.github.io/static/js/3.3332adb5.chunk.js"
+  },
+  {
+    "revision": "dab26a03637721888df5",
+    "url": "jevin925.github.io/static/js/4.23779000.chunk.js"
+  },
+  {
+    "revision": "ab1967473d5317d63ae9",
+    "url": "jevin925.github.io/static/js/main.2815c2c8.chunk.js"
+  },
+  {
+    "revision": "4f6baef207ccd2ed0aa7",
+    "url": "jevin925.github.io/static/js/runtime-main.7b8795bd.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "jevin925.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "jevin925.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "jevin925.github.io/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "jevin925.github.io/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "40bf36f12c25fd021a693bfaf7185072",
+    "url": "jevin925.github.io/static/media/profileMan.40bf36f1.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "jevin925.github.io/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "jevin925.github.io/static/media/whatido.c1a95f73.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3f166ec1c6e5795652507a502703783.js"
+  "jevin925.github.io/precache-manifest.e810f26eac40735056bcb546d699d08b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("jevin925.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
