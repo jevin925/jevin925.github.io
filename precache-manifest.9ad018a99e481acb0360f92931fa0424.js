28c919c2520d5049e6a150fc5209b01a3b1097bf
Jevin Vekaria <jevin925@gmail.com>
2020-07-29 16:49:35 +0530

    first commit

---
+++ precache-manifest.9ad018a99e481acb0360f92931fa0424.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c5a752727fbd03412e15395e1dc39a",
+    "url": "jevin925.github.io/index.html"
+  },
+  {
+    "revision": "9f7013da52aa636b26f6",
+    "url": "jevin925.github.io/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "aef02e546db81326e55e",
+    "url": "jevin925.github.io/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "005dd4ecaa47731d037e",
+    "url": "jevin925.github.io/static/css/main.ac0528b8.chunk.css"
+  },
+  {
+    "revision": "76c32c743f29df5f8c9c",
+    "url": "jevin925.github.io/static/js/2.bde7e412.chunk.js"
+  },
+  {
+    "revision": "9f7013da52aa636b26f6",
+    "url": "jevin925.github.io/static/js/3.3332adb5.chunk.js"
+  },
+  {
+    "revision": "aef02e546db81326e55e",
+    "url": "jevin925.github.io/static/js/4.dc6e5edc.chunk.js"
+  },
+  {
+    "revision": "005dd4ecaa47731d037e",
+    "url": "jevin925.github.io/static/js/main.b5fc5e3a.chunk.js"
+  },
+  {
+    "revision": "2d099cf3e1c0242a5000",
+    "url": "jevin925.github.io/static/js/runtime-main.a4457097.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "jevin925.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "jevin925.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "jevin925.github.io/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "jevin925.github.io/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "jevin925.github.io/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "jevin925.github.io/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "jevin925.github.io/static/media/skill.34409398.svg"
+  }
+]);
