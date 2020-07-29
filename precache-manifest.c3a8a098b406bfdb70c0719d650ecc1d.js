0467504c61727f0f24c2b6e12fb741a026bcfc1d
Jevin Vekaria <jevin925@gmail.com>
2020-07-29 12:57:18 +0530

    initial commit

---
+++ precache-manifest.c3a8a098b406bfdb70c0719d650ecc1d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba79e2eae4988950daa12339039ec9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a6ef53e945932c9e0cd",
+    "url": "/static/css/3.48a93ee6.chunk.css"
+  },
+  {
+    "revision": "dab26a03637721888df5",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "2f54d0d11e0fe216b3b8",
+    "url": "/static/css/main.5b16aff8.chunk.css"
+  },
+  {
+    "revision": "76c32c743f29df5f8c9c",
+    "url": "/static/js/2.bde7e412.chunk.js"
+  },
+  {
+    "revision": "6a6ef53e945932c9e0cd",
+    "url": "/static/js/3.3332adb5.chunk.js"
+  },
+  {
+    "revision": "dab26a03637721888df5",
+    "url": "/static/js/4.23779000.chunk.js"
+  },
+  {
+    "revision": "2f54d0d11e0fe216b3b8",
+    "url": "/static/js/main.bbf2cc04.chunk.js"
+  },
+  {
+    "revision": "1d8ad118c0ce34d32496",
+    "url": "/static/js/runtime-main.062ad5cf.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "40bf36f12c25fd021a693bfaf7185072",
+    "url": "/static/media/profileMan.40bf36f1.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/whatido.c1a95f73.svg"
+  }
+]);
